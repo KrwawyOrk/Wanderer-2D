aa86ef9f0135731436360a86349c6ea8eae6f334
KrwawyOrk <rajzendre@gmail.com>
2020-12-23 23:21:38 +0100

    improving the code for more readable

--- StaticMapItem.cpp
+++ StaticMapItem.cpp
@@ -22,23 +22,23 @@ void StaticMapItem::Draw( void )
 	int spriteWidth = m_sprite.GetSDLSurface()->w;
 	int spriteHeigh = m_sprite.GetSDLSurface()->h;
 
-	int x = static_cast<int>( flposition_x - Globals::camera->GetCameraX() );
-	int y = static_cast<int>( flposition_y - Globals::camera->GetCameraY() - m_sprite.GetSDLSurface()->h + Globals::tilesize );
+	int draw_x = static_cast<int>( flposition_x - Globals::camera->GetCameraX() );
+	int draw_y = static_cast<int>( flposition_y - Globals::camera->GetCameraY() - m_sprite.GetSDLSurface()->h + Globals::tilesize );
 	
 	if( spriteWidth == Globals::tilesize )
 	{
-		m_sprite.Draw( Globals::screen, static_cast<int>( flposition_x - Globals::camera->GetCameraX() ), static_cast<int>( flposition_y - Globals::camera->GetCameraY() - m_sprite.GetSDLSurface()->h + Globals::tilesize ) );
+		m_sprite.Draw( Globals::screen, draw_x, draw_y );
 	}
 
 	else if( spriteWidth > Globals::tilesize )
 	{
 		SDL_Rect src;
-		src.x = Globals::tilesize * int(((SDL_GetTicks() / Globals::spriteAnimationSpeed) % 6 ));
+		src.x = Globals::tilesize * int(((SDL_GetTicks() / Globals::spriteAnimationSpeed) % ( spriteWidth / Globals::tilesize ) ));
 		src.y = 0;
 		src.w = Globals::tilesize;
 		src.h = spriteHeigh;
 
-		m_sprite.Draw( Globals::screen, x, y, &src );
+		m_sprite.Draw( Globals::screen, draw_x, draw_y, &src );
 	}
 }
 
